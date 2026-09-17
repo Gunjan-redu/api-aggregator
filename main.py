885cf3168a3057b16741dee423a1689aed80a01f
Gunjan <gunjansingh2033@gmail.com>
2026-09-17 19:33:50 +0530

    feat: geocoding, any city supported,

--- main.py
+++ main.py
@@ -11,20 +11,19 @@ class WeatherOut(BaseModel):
     windspeed: float
 
 
+def get_coordinates(city:str) -> tuple[float, float, str]:
+    res = httpx.get(f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1").json()
+    if not res.get("results"):
+        raise HTTPException(status_code=404, detail="Unknown City")
 
-CITIES = {
-    "Modinagar":   (28.83, 77.58) ,
-    "Delhi":      (28.61, 77.21)  ,
-    "London":    (51.50, 0.12)
-}
-
+    lat = res["results"][0]["latitude"]
+    lon = res["results"][0]["longitude"]
+    name= res ["results"] [0] ["name"]
+    return lat, lon, name
 
 @app.get("/weather", response_model= WeatherOut)
 def get_weather(city :str):
     city = city.title()
-    if  city not in CITIES:
-        raise HTTPException(status_code=404, detail=f"Unknown city. Supported: {', '.join(CITIES)}")
-    lat = CITIES[city][0]
-    long = CITIES[city][1]
-    r = httpx.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current_weather=true")
-    return {"city": city, **r.json()["current_weather"]}
+    lat, lon, name = get_coordinates(city)
+    r = httpx.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true")
+    return {"city": name, **r.json()["current_weather"]}
